b7f36166ecf027def208831ed0a07a1349f66c59
lindocherosogabriel-cyber <lindocherosogabriel@gmail.com>
2026-04-22 17:24:37 -0300

    DOOM IN YOUTUBE VIDEO PLAYER V1

--- main.js
+++ main.js
@@ -1,11 +1,46 @@
-const ytVideoPlayer = document.querySelector(".ytd-watch-flexy");
+const escapePolicy = window.trustedTypes.createPolicy("default", {
+  createHTML: (string) => string // Aqui você poderia filtrar o HTML, mas vamos passar direto
+});
+
+const ytVideoPlayer = document.querySelector(".video-stream");
 
 const IFRAME = document.createElement("iframe");
 
-IFRAME.src = "https://js-dos.com/DOOM/";
+const html = ` 1 <!doctype html>
+ 2 <html lang="en-us">
+ 3   <head>
+ 4     <meta charset="utf-8">
+ 5     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+ 6     <title>DOOM</title>
+ 7     <style type="text/css">
+ 8       .dosbox-container { width: 320px; height: 200px; }
+ 9       .dosbox-container > .dosbox-overlay { background: url(https://js-dos.com/cdn/DOOM.png); }
+10     </style>
+11   </head>
+12   <body>
+13     <div id="dosbox"></div>
+14     <br/>
+15     <button onclick="dosbox.requestFullScreen();">Make fullscreen</button>
+16     
+17     <script type="text/javascript" src="https://js-dos.com/cdn/js-dos-api.js"></script>
+18     <script type="text/javascript">
+19       var dosbox = new Dosbox({
+20         id: "dosbox",
+21         onload: function (dosbox) {
+22           dosbox.run("upload/DOOM-@evilution.zip", "./doom");
+23         },
+24         onrun: function (dosbox, app) {
+25           console.log("App '" + app + "' is runned");
+26         }
+27       });
+28     </script>
+29   </body>
+30 </html>`
+
+IFRAME.srcdoc = escapePolicy.createHTML(html);
 IFRAME.allowFullscreen = true;
-IFRAME.style.width = ytVideoPlayer.style.width;
-IFRAME.style.height = ytVideoPlayer.style.height;
+IFRAME.style.width = "900px";
+IFRAME.style.height = "700px";
 
 
 ytVideoPlayer.replaceWith(IFRAME);
